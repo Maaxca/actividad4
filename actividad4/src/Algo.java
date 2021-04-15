710ea0da68e0d7616c5edd142f155142e9ff4b03
Mario <maaca@gmail.com>
2021-04-15 09:12:36 +0200

    Clase Algo 2ªParte

--- actividad4/src/Algo.java
+++ actividad4/src/Algo.java
@@ -3,5 +3,7 @@ import java.io.IOException;
 public class Algo {
 	public static void main(String[] args) throws IOException {
 		System.out.println("Pues nada bienvenido a algo");
+		System.out.println("cambios esta o no esta");
+
 	}
 }
